863bee3a6bdd81676b0682e65658d5c48b1fe08b
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2024-03-09 11:26:54 +0530

    Project created

--- one/src/main.ts
+++ one/src/main.ts
@@ -52,7 +52,93 @@ let person: {
   age: number;
 } = { name: "kunal kumar", age: 22 };
 
-// console.log(person); 
+// console.log(person);
 
 // Ninth
 
+let car: { make: String; year: number } = {
+  make: "Audi",
+  year: 2022,
+};
+// console.log(car);
+
+// Tenth
+
+let student: {
+  name: String;
+  age: number;
+  isEnrolled: boolean;
+} = { name: "Kunal", age: 22, isEnrolled: true };
+
+// Eleventh
+
+
+let personDetails :{
+  name: String;
+  age: number;
+  hobbies: String[];
+  address : {
+    street : String,
+    city : String
+  };
+  isStudent : boolean;
+  scores: number[];
+  extraInfo :null
+} = {
+  name: "Kunal",
+  age: 22,
+  hobbies: ["Coding", "Travelling", "Reading"],
+  address: {
+    street: "123 Main St",
+    city: "Patna",
+  },
+  isStudent: false,
+  scores: [1, 2, 3, 4, 5],
+  extraInfo: null,
+}
+
+console.log(personDetails);
+
+
+// twelveth
+
+ // interSection TypeScript 
+
+type persontype = { name: String, age: number};
+
+const person2 : persontype = {
+  name: "Kunal",
+  age: 22
+}
+
+type student = persontype & { studentId: number};
+
+const student1 : student = {
+  name: "Kunal",
+  age: 22,
+  studentId: 8
+}
+
+type teacher = persontype &  { subject: String};
+
+const teacher : teacher = { 
+  name: "Kunal",
+  age: 22,
+  subject: "Maths"
+}
+
+
+// thirteenth
+
+let id  : string | number = 123;
+
+// fourteenth
+
+type studentEn = ( number | string )[];
+
+const studentEnrolled : studentEn = [1, 2, 3, 4, 5, "Kunal", "Neha", "Jay"];
+
+// console.log(studentEnrolled);
+
+
+// fifteenth
