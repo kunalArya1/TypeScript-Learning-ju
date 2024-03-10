1251e741730e45751229b88178d9490096573bb2
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2024-03-10 10:43:46 +0530

    Basic type in typeScript

--- one/src/main.ts
+++ one/src/main.ts
@@ -6,4 +6,53 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   </div>
 `;
 
-console.log("hello from typescript");
+console.log("TypeScript Practice Session");
+
+// First
+let variableName: number;
+variableName = 42;
+// console.log(variableName);
+
+// Second
+let myName: String;
+myName = "Kunal Kumar Arya";
+// console.log(myName);
+
+// Third
+let isStudent: boolean;
+isStudent = true;
+// console.log(isStudent);
+
+// Fourth
+
+let myData: null;
+myData = null;
+
+// Fifth
+
+let arrayName: String[];
+arrayName = ["kunal", "Neha", "Jay"];
+// console.log(arrayName);
+
+// Sixth
+
+let fruits: String[];
+fruits = ["Apple", "Banana", "Mango"];
+// console.log(fruits);
+
+// Seventh
+
+let number: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// number.map((num) => console.log(num));
+
+// Eighth
+
+let person: {
+  name: String;
+  age: number;
+} = { name: "kunal kumar", age: 22 };
+
+// console.log(person); 
+
+// Ninth
+
